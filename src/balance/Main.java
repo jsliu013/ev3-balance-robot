ea97bfbc3259eee2a880ce3d373ae822d2d67b6e
Ryan Hubbard <ryandhubbard@gmail.com>
2018-02-03 13:20:04 -0800

    Initialize motors and gyro

--- src/balance/Main.java
+++ src/balance/Main.java
@@ -1,43 +1,37 @@
 package balance;
 
-import lejos.hardware.*;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
-import lejos.hardware.sensor.*;
-import lejos.robotics.EncoderMotor;
-import lejos.robotics.Gyroscope;
-import lejos.hardware.motor.*;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.port.MotorPort;
+
 
 public class Main {
-
-	public Gyroscope gyro; 
-	public static EncoderMotor left_motor;
-	public static EncoderMotor right_motor;
 	
-	public static void main(String[] args) {
+	static EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.A);
+	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.B);
+	static EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S1);
+	
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		EV3 ev3 = (EV3) BrickFinder.getLocal();
 		TextLCD lcd = ev3.getTextLCD();
 		Keys keys = ev3.getKeys();
 		
-		
 		lcd.drawString("Press key to start", 4, 4);
 		keys.waitForAnyPress();
 
 		  while(true) {
-//			     GetGyroData(gyroSpeed, gyroAngle);
-//			     GetMotorData(motorSpeed, motorPos);
-//
-//			     power = KGYROSPEED * gyroSpeed +
-//			             KGYROANGLE * gyroAngle +
-//			             KPOS       * motorPos  +
-//			             KSPEED     * motorSpeed;
-//
-//			     OnFwd(left_motor, power);
-//			     OnFwd(right_motor, power);
-//			     
-//			     wait(7);
+			    
+			    RIGHT_MOTOR.forward();
+			    LEFT_MOTOR.forward();
+
+			  Thread.sleep(7);
 		  }
 	}
 }
